ce0a88cf099578cecbd497d5930f14d6ebe27469
bartosz-gieron <58327656+bartosz-gieron@users.noreply.github.com>
2020-02-13 14:14:51 +0100

    Add files via upload

---
+++ kompresja/kompresja/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace kompresja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Opnbutton = new System.Windows.Forms.Button();
+            this.formatBox = new System.Windows.Forms.ComboBox();
+            this.levelBox = new System.Windows.Forms.ComboBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.Cnvbutton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Controls.Add(this.Opnbutton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.formatBox, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.levelBox, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Cnvbutton, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 400);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 378F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 378F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 47);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Opnbutton
+            // 
+            this.Opnbutton.Location = new System.Drawing.Point(3, 3);
+            this.Opnbutton.Name = "Opnbutton";
+            this.Opnbutton.Size = new System.Drawing.Size(110, 35);
+            this.Opnbutton.TabIndex = 5;
+            this.Opnbutton.Text = "Open";
+            this.Opnbutton.UseVisualStyleBackColor = true;
+            this.Opnbutton.Click += new System.EventHandler(this.Opnbutton_Click_1);
+            // 
+            // formatBox
+            // 
+            this.formatBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.formatBox.FormattingEnabled = true;
+            this.formatBox.Items.AddRange(new object[] {
+            "JPEG",
+            "GIF",
+            "TIFF",
+            "PNG",
+            "BMP"});
+            this.formatBox.Location = new System.Drawing.Point(545, 3);
+            this.formatBox.Name = "formatBox";
+            this.formatBox.Size = new System.Drawing.Size(110, 21);
+            this.formatBox.TabIndex = 2;
+            this.formatBox.Tag = "";
+            this.formatBox.SelectedIndexChanged += new System.EventHandler(this.FormatBox_SelectedIndexChanged);
+            // 
+            // levelBox
+            // 
+            this.levelBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.levelBox.FormattingEnabled = true;
+            this.levelBox.Location = new System.Drawing.Point(661, 3);
+            this.levelBox.Name = "levelBox";
+            this.levelBox.Size = new System.Drawing.Size(112, 21);
+            this.levelBox.TabIndex = 3;
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(119, 3);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(304, 20);
+            this.textBox.TabIndex = 4;
+            // 
+            // Cnvbutton
+            // 
+            this.Cnvbutton.Location = new System.Drawing.Point(429, 3);
+            this.Cnvbutton.Name = "Cnvbutton";
+            this.Cnvbutton.Size = new System.Drawing.Size(110, 35);
+            this.Cnvbutton.TabIndex = 3;
+            this.Cnvbutton.Text = "Convert";
+            this.Cnvbutton.UseVisualStyleBackColor = true;
+            this.Cnvbutton.Click += new System.EventHandler(this.Cnvbutton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(776, 362);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.PictureBox_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label.Location = new System.Drawing.Point(12, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox formatBox;
+        private System.Windows.Forms.ComboBox levelBox;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button Opnbutton;
+        private System.Windows.Forms.Button Cnvbutton;
+    }
+}
+
---
+++ kompresja/kompresja/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace kompresja
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Opnbutton_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fld = new OpenFileDialog())
+            {
+                if (fld.ShowDialog() == DialogResult.OK)
+                {
+                    textBox.Text = fld.FileName.ToString();
+                    Bitmap image = new Bitmap(fld.FileName);
+                    pictureBox.Image = image;
+                }
+                else MessageBox.Show("nie wybrano pliku"); 
+            }
+            
+        }
+
+        private void Cnvbutton_Click(object sender, EventArgs e)
+        {
+            count++;
+            label.Text = "";
+            string level;
+            if (levelBox.SelectedItem == null)
+                level = "0";
+            else
+                level = levelBox.SelectedItem.ToString();
+
+            if (formatBox.Text.ToString() != "")
+                Conv(formatBox.SelectedItem.ToString(),
+                    textBox.Text.ToString(),
+                    level);
+            else
+                MessageBox.Show("Nie wybrano formatu");
+        }
+
+        private void Conv(string format, string filepath, string level)
+        {
+            Bitmap bmp = new Bitmap(filepath);
+            string path = "C:\\Users\\Bartek\\Desktop\\kompresja\\image" + count.ToString() + "." + format.ToLower();
+
+            EncoderParameter enc = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality,100);
+
+            if (Int32.TryParse(level, out int int_level))
+               enc = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality,int_level);
+            else
+                switch (level.ToUpper())
+                {
+                    case "LZW":
+                        enc = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression,
+                            (long)EncoderValue.CompressionLZW);
+                        break;
+                    case "CCITT3":
+                        enc = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression,
+                            (long)EncoderValue.CompressionCCITT3);
+                        break;
+                    case "CCITT4":
+                        enc = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression,
+                            (long)EncoderValue.CompressionCCITT4);
+                        break;
+                    case "RLE":
+                        enc = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression,
+                            (long)EncoderValue.CompressionRle);
+                        break;
+                    case "NONE":
+                        enc = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression,
+                            (long)EncoderValue.CompressionNone);
+                        break;
+                }
+
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+            myEncoderParameters.Param[0] = enc;
+
+            ImageCodecInfo myImageCodecInfo;
+            myImageCodecInfo = GetEncoderInfo("image/" + format.ToLower());
+
+            using (Bitmap image = new Bitmap(bmp))
+            {
+                try
+                {
+                    switch (format)
+                    {
+                        case "PNG":
+                                image.Save(path, ImageFormat.Png);
+                            break;
+                        case "BMP":
+                            image.Save(path, ImageFormat.Bmp);
+                            break;
+                        case "JPEG":
+                            if (level == "0")
+                                image.Save(path, ImageFormat.Jpeg);
+                            else
+                                image.Save(path, myImageCodecInfo, myEncoderParameters);
+                            break;
+                        case "TIFF":
+                            if (level == "0")
+                                image.Save(path, ImageFormat.Tiff);
+                            else
+                                image.Save(path, myImageCodecInfo, myEncoderParameters);
+                            break;
+                        case "GIF":
+                            image.Save(path, ImageFormat.Gif);
+                            break;
+                    }
+                    label.Text = "Kompresja przebiegła pomyślnie!";
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+                Bitmap toDisplayAfterCompress = new Bitmap(path);
+                pictureBox.Image = toDisplayAfterCompress;
+                
+                
+            }
+        }
+
+        private void FormatBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            levelBox.Items.Clear();
+            if (formatBox.Text.ToString() == "JPEG")
+                levelBox.Items.AddRange(new object[] { "5", "25", "50", "100" });
+            else if (formatBox.Text.ToString() == "TIFF")
+                levelBox.Items.AddRange(new object[] { "LZW", "CCITT3", "CCITT4", "Rle", "None" });
+            else
+                levelBox.Items.Clear();
+
+        }
+        private ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
